505c095df020232aa9066289d96e4a854dd42c7d
NathanaelBenjamin <86367360+NathanaelBenjamin@users.noreply.github.com>
2022-01-08 16:01:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+//GETTING ALL DOM Objects.
+
+const form = document.querySelector('form');
+const city = document.getElementById('city');
+const submitButton = document.getElementById('btn');
+const weatherDisplay = document.querySelector('.weather');
+const specifiedLocation = document.querySelector('.location');
+const temperature = document.querySelector('.temperature');
+const mainWeather = document.querySelector('.main-weather');
+const locationIcon = document.querySelector('#location-icon');
+const alert = document.querySelector('.alert');
+
+form.addEventListener('submit' ,(e) => {
+    e.preventDefault( );
+
+    const weather = async ( ) => {
+
+        var cityName = city.value.toLowerCase( );
+        const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&APPID=b34fddd3dae4a2eb0ad363b62f98ba1e');
+        if(response.status !== 200 || response.status == 404){ 
+            throw new Error('Error fetching data');
+        }
+
+       
+    
+        const data = await response.json( );
+        return data;
+    };
+    
+    weather( ).then(data => {
+        alert.style.display = 'none';
+        let temperatureInCelcius = Math.floor(data.main.temp - 273);
+        specifiedLocation.innerText = (city.value + ", " + data.sys.country + '.');
+        temperature.innerText = temperatureInCelcius + "'C";
+        mainWeather.innerText = (data.weather[0].main + ', ' + data.weather[0].description + '.');
+        locationIcon.style.display = 'block';
+        weatherDisplay.style.display ="block";
+
+    }).catch(error => {
+        console.log(`Promise rejected: `, error.message);
+       
+         alert.style.display = 'block';
+         weatherDisplay.style.display ="none";
+        
+    });
+});
